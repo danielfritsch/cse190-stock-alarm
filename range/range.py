be3f67c7511dad6fcd0e782dddf31151721d8b5a
Geon Yoo <46584775+jasongeon123@users.noreply.github.com>
2021-05-31 18:34:20 -0700

    updated range 1

--- range/range.py
+++ range/range.py
@@ -5,29 +5,30 @@ warnings.filterwarnings("ignore")
 import numpy as np
 import sklearn
 from sklearn.ensemble import IsolationForest
-#degree "low", "medium","high"
-data = pd.read_csv("./AAPL.csv")
-def machine_learning(tickr):
+import os
+import sys
+data = pd.read_csv(open(os.path.join(sys.path[0], "AAPL.csv"), "r"))
+def getRange(tickr):
     if(tickr=="AAPL"):
-        data = pd.read_csv("./AAPL.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "AAPL.csv"), "r"))
     elif(tickr == "PANW"):
-        data = pd.read_csv("./PANW.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "PANW.csv"), "r"))
     elif(tickr == "AMZN"):
-        data = pd.read_csv("./AMZN.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "AMZN.csv"), "r"))
     elif(tickr == "BABA"):
-        data = pd.read_csv("./BABA.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "BABA.csv"), "r"))
     elif(tickr == "FB"):
-        data = pd.read_csv("./FB.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "FB.csv"), "r"))
     elif(tickr == "MSFT"):
-        data = pd.read_csv("./MSFT.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "MSFT.csv"), "r"))
     elif(tickr == "NFLX"):
-        data = pd.read_csv("./NFLX.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "NFLX.csv"), "r"))
     elif(tickr == "NVDA"):
-        data = pd.read_csv("./NVDA.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "NVDA.csv"), "r"))
     elif(tickr == "SQ"):
-        data = pd.read_csv("./SQ.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "SQ.csv"), "r"))
     elif(tickr == "TSLA"):
-        data = pd.read_csv("./TSLA.csv")
+        data = pd.read_csv(open(os.path.join(sys.path[0], "TSLA.csv"), "r"))
     else:
         return "Coming Soon"
 
@@ -40,10 +41,9 @@ def machine_learning(tickr):
     clf = IsolationForest(random_state=0,n_jobs=-1).fit(data)
     i=0
     a_range = []
-    final = []
     while(i<=100):
         if(clf.predict([[i]])==1):
             a_range.append(i)
         i = i+0.1
-    final = [a_range[0],a_range[-1]]
+    final = a_range[-1]
     return final
