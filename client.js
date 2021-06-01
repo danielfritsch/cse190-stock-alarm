ae01052ab286a3aba06ac8373faab80c8aaebce3
Anh Mac <anhnmac@gmail.com>
2021-05-31 17:13:56 -0700

    restruc

--- client.js
+++
@@ -1,9 +0,0 @@
-const fetch = require("node-fetch");
-
-const getPercentChange = () => {
-	fetch('http://127.0.0.1:5000/percent_change/AAPL')
-  		.then(response => response.text())
-  		.then(data => console.log(data));	
-}
-
-getPercentChange();
